afd294367debd597e1960c83b97297def57b32e5
Nived kanwar <nivedkanwar@live.com>
2026-05-10 23:37:45 +0530

    Commit 1 - Code refactored based on Strategy pattern. The existing code is based on quantity checks and item subchecks and new design will help in extending/adding any new item without shaking whole setup.

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace GildedRose.Console;
 
@@ -12,8 +13,8 @@ public class Program
 
         var app = new Program()
                       {
-                          Items = new List<Item>
-                                      {
+                          Items =
+                                      [
                                           new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                                           new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                                           new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -25,7 +26,7 @@ public class Program
                                                   Quality = 20
                                               },
                                           new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                      }
+                                      ]
 
                       };
 
@@ -36,77 +37,10 @@ public class Program
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < Items.Count; i++)
+        foreach (var item in Items)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                }
-            }
+            var updater = ItemUpdaterFactory.Create(item);
+            updater.Update(item);
         }
     }
 }
@@ -118,4 +52,113 @@ public class Item
     public int SellIn { get; set; }
 
     public int Quality { get; set; }
+}
+
+internal class SulfurasUpdater : IItemUpdater
+{
+    public void Update(Item item)
+    {
+        // Legendary: no changes to SellIn or Quality
+    }
+}
+
+internal class BackstageUpdater : IItemUpdater
+{
+    public void Update(Item item)
+    {
+        if (item.SellIn <= 0)
+        {
+            // after the concert
+            item.Quality = 0;
+        }
+        else if (item.SellIn <= 5)
+        {
+            ItemHelpers.IncreaseQuality(item, 3);
+        }
+        else if (item.SellIn <= 10)
+        {
+            ItemHelpers.IncreaseQuality(item, 2);
+        }
+        else
+        {
+            ItemHelpers.IncreaseQuality(item, 1);
+        }
+
+        item.SellIn--;
+    }
+}
+
+internal class AgedBrieUpdater : IItemUpdater
+{
+    public void Update(Item item)
+    {
+        ItemHelpers.IncreaseQuality(item, 1);
+        item.SellIn--;
+        if (item.SellIn < 0)
+        {
+            ItemHelpers.IncreaseQuality(item, 1);
+        }
+    }
+}
+
+internal class NormalUpdater : IItemUpdater
+{
+    public void Update(Item item)
+    {
+        ItemHelpers.DecreaseQuality(item, 1);
+        item.SellIn--;
+        if (item.SellIn < 0)
+        {
+            ItemHelpers.DecreaseQuality(item, 1);
+        }
+    }
+}
+
+internal class ConjuredUpdater : IItemUpdater
+{
+    // Conjured items degrade twice as fast as normal items:
+    // - before sell date: 2 per day
+    // - after sell date: 4 per day
+    public void Update(Item item)
+    {
+        ItemHelpers.DecreaseQuality(item, 2);
+        item.SellIn--;
+        if (item.SellIn < 0)
+        {
+            ItemHelpers.DecreaseQuality(item, 2);
+        }
+    }
+}
+
+internal static class ItemHelpers
+{
+    private const int MaxQuality = 50;
+    private const int MinQuality = 0;
+
+    public static void IncreaseQuality(Item item, int amount = 1)
+    {
+        item.Quality = Math.Min(MaxQuality, item.Quality + Math.Max(0, amount));
+    }
+
+    public static void DecreaseQuality(Item item, int amount = 1)
+    {
+        item.Quality = Math.Max(MinQuality, item.Quality - Math.Max(0, amount));
+    }
+}
+
+public interface IItemUpdater
+{
+    void Update(Item item);
+}
+
+public static class ItemUpdaterFactory
+{
+    public static IItemUpdater Create(Item item)
+    {
+        if (item.Name == "Sulfuras, Hand of Ragnaros") return new SulfurasUpdater();
+        if (item.Name == "Aged Brie") return new AgedBrieUpdater();
+        if (item.Name.Contains("Backstage") || item.Name.Contains("Backstage passes")) return new BackstageUpdater();
+        if (item.Name.Contains("Conjured")) return new ConjuredUpdater();
+        return new NormalUpdater();
+    }
 }
